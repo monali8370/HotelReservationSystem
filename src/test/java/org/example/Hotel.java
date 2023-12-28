99b9d8e071442c1398441206f810420041441b81
monali8370 <monalishinde952@gmail.com>
2023-12-28 22:17:21 +0530

    ability:to add hotel

--- src/test/java/org/example/Hotel.java
+++ src/test/java/org/example/Hotel.java
@@ -1,7 +1,37 @@
 package org.example;
 
+import java.util.List;
+
 public class Hotel {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Hotel Reservation Program");
+    String hotelName;
+    int hotelWeekDayRegularCustomer;
+
+    public Hotel(String hotelName, int hotelWeekDayRegularCustomer) {
+        this.hotelName = hotelName;
+        this.hotelWeekDayRegularCustomer = hotelWeekDayRegularCustomer;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public int getHotelWeekDayRegularCustomer() {
+        return hotelWeekDayRegularCustomer;
+    }
+
+    public void setHotelWeekDayRegularCustomer(int hotelWeekDayRegularCustomer) {
+        this.hotelWeekDayRegularCustomer = hotelWeekDayRegularCustomer;
+    }
+
+    @Override
+    public String toString() {
+        return "Hotel{" +
+                "hotelName='" + hotelName + '\'' +
+                ", hotelWeekDayRegularCustomer=" + hotelWeekDayRegularCustomer +
+                '}';
     }
 }
